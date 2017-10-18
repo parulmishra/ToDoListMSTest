7a303e4b8b3c712f61d468b73548dfd7b1163ba9
Guest User <Guest@epicodus-79.local>
2017-10-18 14:16:15 -0700

    this commit fix issues

--- todolist.Tests/ControllerTests/ItemsControllerTest.cs
+++ todolist.Tests/ControllerTests/ItemsControllerTest.cs
@@ -3,20 +3,37 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using ToDoList.Controllers;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using Moq;
 
 namespace ToDoList.Tests                  
 {   
-    [TestClass]
-    public class ItemsControllerTest
+    
+    [TestClass]  
+    public class ItemsControllerTest: IDisposable
     {
-        [TestMethod]
-        public void GetDescriptionTest()
+        // Arrange
+        Mock<IItemRepository> mock = new Mock<IItemRepository>();
+        EFItemRepository db = new EFItemRepository(new TestDbContext());
+
+        private void DbSetup()
         {
+            mock.Setup(m => m.Items).Returns(new Item[]
+            {
+                new Item {ItemId = 1, Description = "Wash the dog" },
+                new Item {ItemId = 2, Description = "Do the dishes" },
+                new Item {ItemId = 3, Description = "Sweep the floor" }
+            }.AsQueryable());
+        }
+
+        [TestMethod]
+        public void Mock_GetViewResultIndex_Test() //Confirms route returns view
+		{
             //Arrange
-            ItemsController controller = new ItemsController();
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
             //Act
             var result = controller.Index();
             //Assert
@@ -24,10 +41,11 @@ namespace ToDoList.Tests
         }
 
         [TestMethod]
-        public void Get_ModelList_Index_Test()
-        {
+        public void Mock_IndexListOfItems_Test() //Confirms model as list of items
+		{
 			//Arrange
-			ViewResult indexView = new ItemsController().Index() as ViewResult;
+			DbSetup();
+			ViewResult indexView = new ItemsController(mock.Object).Index() as ViewResult;
 
 			//Act
 			var result = indexView.ViewData.Model;
@@ -36,20 +54,41 @@ namespace ToDoList.Tests
 			Assert.IsInstanceOfType(result, typeof(List<Item>));
         }
         [TestMethod]
-        public void Post_MethodAddsItem_Test()
-        {
+        public void Mock_ConfirmEntry_Test() //Confirms presence of known entry
+		{
             //Arrange
-            ItemsController controller = new ItemsController();
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
             Item testItem = new Item();
             testItem.Description = "test item";
+			testItem.ItemId = 1;
 
-            //Act
-            controller.Create(testItem);
-            ViewResult indexView = new ItemsController().Index() as ViewResult;
-            var collection = indexView.ViewData.Model as List<Item>;
+			// Act
+			ViewResult indexView = controller.Index() as ViewResult;
+			var collection = indexView.ViewData.Model as List<Item>;
 
             //Assert
             CollectionAssert.Contains(collection,testItem);
+        }
+		[TestMethod]
+        public void DB_CreateNewEntry_Test()
+		{
+			// Arrange
+			ItemsController controller = new ItemsController(db);
+			Item testItem = new Item();
+			testItem.Description = "TestDb Item";
+
+			// Act
+			controller.Create(testItem);
+			var collection = (controller.Index() as ViewResult).ViewData.Model as List<Item>;
+
+			// Assert
+			CollectionAssert.Contains(collection, testItem);
+		}
+
+        public void Dispose()
+        {
+            mock.Object.DeleteAll();
         }
     }
 }
--- todolist.Tests/Models/TestDbContext.cs
+++ todolist.Tests/Models/TestDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-namespace todolist.Tests.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
 {
-    public class TestDbContext
+    public class TestDbContext : ToDoListContext
     {
-        public TestDbContext()
-        {
-        }
+		public override DbSet<Item> Items { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+            options.UseMySql(@"Server=localhost;Port=8889;database=to_do_list_test;uid=root;pwd=root;");
+		}
     }
 }
